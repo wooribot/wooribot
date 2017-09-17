a0cef0f6995fa0e35e102e55e5ebcd9918504001
unamedkr <unamed.kr@gmail.com>
2017-09-18 01:39:52 +0900

    ,

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-/*
-  CONGRATULATIONS on creating your first Botpress bot!
-
-  This is the programmatic entry point of your bot.
-  Your bot's logic resides here.
-
-  Here's the next steps for you:
-  1. Read this file to understand how this simple bot works
-  2. Read the `content.yml` file to understand how messages are sent
-  3. Install a connector module (Facebook Messenger and/or Slack)
-  4. Customize your bot!
-
-  Happy bot building!
-
-  The Botpress Team
-  ----
-  Getting Started (Youtube Video): https://www.youtube.com/watch?v=HTpUmDz9kRY
-  Documentation: https://botpress.io/docs
-  Our Slack Community: https://slack.botpress.io
-*/
 
 module.exports = function(bp) {
   bp.rivescript.setUtf8(true)
